ec1ee5f9f35c5814762a99a7587258661083ce79
riest817 <sano817@hotmail.co.jp>
2020-03-11 17:56:27 +0900

    game add title, HP, MP, enemy_image_change

--- work/my_game_project/Script.js
+++ work/my_game_project/Script.js
@@ -1,8 +1,11 @@
 //import Start_End from "./start_end.js";
 enchant();
 
+const Game_width = 400;
+const Game_height = 500;
+
 window.onload = function () {
-	var game = new Game(500, 800);  				//画面サイズを400*500にする。（このサイズだとスマホでも快適なのでおススメ）
+	var game = new Game(Game_width, Game_height);  				//画面サイズを400*500にする。（このサイズだとスマホでも快適なのでおススメ）
 
 	//結果ツイート時にURLを貼るため、このゲームのURLをここに記入
 	var url = "https://twitter.com/hothukurou";
@@ -43,6 +46,7 @@ window.onload = function () {
 		var Point = 0;									//ポイント
 		var Stage = -1;								//現在のゲーム状態
 		var My_HP = 10;
+		var My_MP = 0;
 		var Enemy_HP = 5;
 
 		//グローバル変数終わり
@@ -57,15 +61,15 @@ window.onload = function () {
 		var S_GameSTARTText = new Label(); 					//テキストはLabelクラス
 		S_GameSTARTText.font = "40px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
 		S_GameSTARTText.color = 'rgba(255,255,255,1)';		//色　RGB+透明度　今回は白
-		S_GameSTARTText.width = 400;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
-		S_GameSTARTText.moveTo(120, 150);						//移動位置指定
+		S_GameSTARTText.width = Game_width;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
+		S_GameSTARTText.moveTo(80, 150);						//移動位置指定
 		S_START.addChild(S_GameSTARTText);						//S_STARTシーンにこの画像を埋め込む
 
 		S_GameSTARTText.text = "テストゲーム";	
 
 		//スタートボタン
 		var B_START = new Sprite(120, 60);				//画像サイズをここに書く。使う予定の画像サイズはプロパティで見ておくこと
-		B_START.moveTo(135, 300);						//スタートボタンの位置
+		B_START.moveTo(140, 300);						//スタートボタンの位置
 		B_START.image = game.assets[B_Start];			//読み込む画像の相対パスを指定。　事前にgame.preloadしてないと呼び出せない
 		S_START.addChild(B_START);					//B_STARTにこのスタートボタン画像を貼り付ける  
 
@@ -84,16 +88,25 @@ window.onload = function () {
 		var S_Text = new Label(); 					//テキストはLabelクラス
 		S_Text.font = "20px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
 		S_Text.color = 'rgba(255,255,255,1)';		//色　RGB+透明度　今回は白
-		S_Text.width = 400;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
+		S_Text.width = Game_width;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
 		S_Text.moveTo(0, 440);						//移動位置指定
 		S_MAIN.addChild(S_Text);					//S_MAINシーンにこの画像を埋め込む
 		S_Text.text = "勇者HP：" + My_HP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
+
+		//勇者マジックポイント表示テキスト
+		var M_Text = new Label(); 					//テキストはLabelクラス
+		M_Text.font = "20px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
+		M_Text.color = 'rgba(255,255,255,1)';		//色　RGB+透明度　今回は白
+		M_Text.width = Game_width;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
+		M_Text.moveTo(0, 470);						//移動位置指定
+		S_MAIN.addChild(M_Text);					//S_MAINシーンにこの画像を埋め込む
+		M_Text.text = "勇者MP：" + My_MP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
 
 		//敵ヒットポイント表示テキスト
 		var Enemy_Text = new Label(); 					//テキストはLabelクラス
 		Enemy_Text.font = "20px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
 		Enemy_Text.color = 'rgba(255,255,255,1)';		//色　RGB+透明度　今回は白
-		Enemy_Text.width = 400;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
+		Enemy_Text.width = Game_width;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
 		Enemy_Text.moveTo(0, 30);						//移動位置指定
 		S_MAIN.addChild(Enemy_Text);					//S_MAINシーンにこの画像を埋め込む
 		Enemy_Text.text = "敵HP：" + Enemy_HP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
@@ -108,8 +121,9 @@ window.onload = function () {
 			Point++;									//Pointを1増やす
 			game.assets[ClickSound].clone().play();		//クリックの音を鳴らす。
 
-			//クリックしたのでスライム画像のｘ位置を戻す
-			//this.x = -200;							//this.xって何？と思った方、Slimeの関数内でスライムの座標を動かすときにはthisを使います。
+			//クリックしたのでスライムのHPを減らす
+			Enemy_HP--;
+			Enemy_Text.text = "敵HP：" + Enemy_HP;				//変数が変化するので、毎フレームごとにPointの値を読み込んだ文章を表示する
 
 			//ポイントによって状態Stageを変更する
 			if (Point < 3) {
@@ -122,6 +136,12 @@ window.onload = function () {
 				Stage = 4;
 			} else {
 				Stage = 5;
+			}
+
+			if ( Enemy_HP == 0 ) {
+				game.popScene();					//S_MAINシーンを外す
+				game.pushScene(S_END);				//S_ENDシーンを読み込ませる
+				
 			}
 
 		};
@@ -183,12 +203,13 @@ window.onload = function () {
 
 		//GAMEOVER
 		var S_GameOverText = new Label(); 					//テキストはLabelクラス
-		S_GameOverText.font = "20px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
+		S_GameOverText.font = "40px Meiryo";				//フォントはメイリオ 20px 変えたかったらググってくれ
 		S_GameOverText.color = 'rgba(255,255,255,1)';		//色　RGB+透明度　今回は白
-		S_GameOverText.width = 400;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
-		S_GameOverText.moveTo(0, 30);						//移動位置指定
+		S_GameOverText.width = Game_width;							//横幅指定　今回画面サイズ400pxなので、width:400pxだと折り返して二行目表示してくれる
+		S_GameOverText.moveTo(80, 150);						//移動位置指定
 		S_END.addChild(S_GameOverText);						//S_ENDシーンにこの画像を埋め込む
-
+		//ゲームオーバー後のテキスト表示
+		S_GameOverText.text = "GAMEOVER";				//テキストに文字表示 
 
 
 		//リトライボタン
@@ -199,6 +220,12 @@ window.onload = function () {
 
 		S_Retry.ontouchend = function () {				//S_Retryボタンをタッチした（タッチして離した）時にこの中の内容を実行する
 			Stage = 0;
+			My_HP = 10;
+			S_Text.text = "勇者HP：" + My_HP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
+			My_MP = 0;
+			M_Text.text = "勇者MP：" + My_MP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
+			Enemy_HP = 5;
+			Enemy_Text.text = "敵HP：" + Enemy_HP;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
 			game.popScene();						//S_ENDシーンを外す
 			game.pushScene(S_MAIN);					//S_MAINシーンを入れる
 		};
@@ -212,7 +239,7 @@ window.onload = function () {
 		S_Tweet.ontouchend = function () {				//S_Tweetボタンをタッチした（タッチして離した）時にこの中の内容を実行する
 			//ツイートＡＰＩに送信
 
-			window.open("http://twitter.com/intent/tweet?text=頑張って" + Point + "枚入手した&hashtags=ahoge&url=" + url); //ハッシュタグにahogeタグ付くようにした。
+			window.open("http://twitter.com/intent/tweet?text=頑張って" + Stage + "ステージまで進出した&hashtags=ahoge&url=" + url); //ハッシュタグにahogeタグ付くようにした。
 		};
 
 	};
